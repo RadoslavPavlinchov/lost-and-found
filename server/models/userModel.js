9f1853bd0bf9228deab741f705b477384576462f
Radoslav <radoslav.pavlinchov@gmail.com>
2024-07-07 22:09:59 +0300

    feat: add user model schema

---
+++ server/models/userModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+export default mongoose.model("User", UserSchema);
