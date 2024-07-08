c6f5e7767a3918657e4f9f22ac1fb29d50e58123
Radoslav <radoslav.pavlinchov@gmail.com>
2024-07-08 23:41:47 +0300

    feat: add footer component and styles

---
+++ client/src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css"
+
+export default function Foooter() {
+    return (
+        <footer className={styles.footer}>
+            <p>App Footer</p>
+        </footer>
+    );
+}
