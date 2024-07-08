6002ec0e783eddc4a4f89d5760c0e8d7086904b1
Radoslav <radoslav.pavlinchov@gmail.com>
2024-07-08 23:40:07 +0300

    feat: add home page component and styles

---
+++ client/src/components/pages/Home.tsx
@@ -0,0 +1,10 @@
+import styles from "./Home.module.css"
+
+export default function Home() {
+    return (
+        <div className={styles.home}>
+            <h2>Home</h2>
+            <p>Welcome to the Home page</p>
+        </div>
+    )
+}
