eb8351e9996285b0f20555a67926da28cb227408
Radoslav <radoslav.pavlinchov@gmail.com>
2024-07-08 23:43:52 +0300

    feat: update App component to include authentication and navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,38 @@
-import './App.css'
+import Header from "./components/layout/Header"
+import styles from "./App.module.css"
+import Home from "./components/pages/Home"
+
+import { useState } from "react"
+import Login from "./components/auth/Login"
+import Register from "./components/auth/Register"
+import Footer from "./components/layout/Footer"
 
 function App() {
+	const [currentPage, setCurrentPage] = useState("home")
 
-  return (
-    <>
-    </>
-  )
+	function showPage(): JSX.Element {
+		if (currentPage === "home") {
+			return <Home />
+		}
+
+		if (currentPage === "login") {
+			return <Login />
+		}
+
+		if (currentPage === "register") {
+			return <Register />
+		}
+
+		return <Home />
+	}
+
+	return (
+		<div className={styles.app}>
+			<Header setCurrentPage={setCurrentPage} />
+			<main>{showPage()}</main>
+			<Footer />
+		</div>
+	)
 }
 
 export default App
