bd012f666a3920e44b9f296e8a5767140523765f
Radoslav <radoslav.pavlinchov@gmail.com>
2024-07-10 23:28:06 +0300

    feat: add user registration functionality

--- client/src/components/auth/Register.tsx
+++ client/src/components/auth/Register.tsx
@@ -1,13 +1,31 @@
+import { FormEvent } from 'react';
 import styles from './Auth.module.css';
 
 export default function Register() {
+    const baseUrl = "http://localhost:3000/api/"
+
+    const onRegisterHandler = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const formData = new FormData(e.currentTarget)
+        const userData = Object.fromEntries(formData)
+
+        await fetch(`${baseUrl}users`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(userData)
+        })
+    }
+
     return (
         <div className={styles.auth}>
             <h2>Register</h2>
-            <form>
-                <input type="text" placeholder="Name" required />
-                <input type="email" placeholder="Email" required />
-                <input type="password" placeholder="Password" required />
+            <form onSubmit={onRegisterHandler}>
+                <input type="text" placeholder="Name" name="name" required />
+                <input type="email" placeholder="Email" name="email" required />
+                <input type="password" placeholder="Password" name="password" required />
                 <button type="submit">Register</button>
             </form>
         </div>
---
+++ client/src/interfaces/User.ts
@@ -0,0 +1,6 @@
+export default interface User {
+    name: string;
+    email: string;
+    createdAt: string;
+    _id: string;
+}
