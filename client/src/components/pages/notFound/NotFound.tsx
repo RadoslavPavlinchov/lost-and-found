dbdb3ea416b7ee66fc1f656df951214bc1810b7c
Radoslav <radoslav.pavlinchov@gmail.com>
2024-07-13 16:55:51 +0300

    feat: add 404 page component and styles

---
+++ client/src/components/pages/notFound/NotFound.tsx
@@ -0,0 +1,10 @@
+import styles from "./NotFound.module.css"
+
+export default function NotFound() {
+    return (
+        <div className={styles.home}>
+            <h2>404</h2>
+            <p>Page Not Found</p>
+        </div>
+    )
+}
