8b7b87c4048ddcca2d5fe3a62759431622c0a11a
Radoslav <radoslav.pavlinchov@gmail.com>
2024-07-21 19:08:15 +0300

    feat: include jwt token into headers, add refresh token

--- client/src/app/api/apiSlice.ts
+++ client/src/app/api/apiSlice.ts
@@ -1,7 +1,43 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { setCredentials } from './authSlice'
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: 'http://localhost:3000/api',
+    credentials: "include",
+    prepareHeaders: (headers, { getState }) => {
+        const token = getState().auth.token
+
+        if (token) {
+            headers.set('authorization', `Bearer ${token}`)
+        }
+
+        return headers
+    }
+})
+
+const baseQueryWithRefreshAuth = async (args, api, extraOptions) => {
+    let result = await baseQuery(args, api, extraOptions)
+
+    if (result?.error?.status === 403) {
+        const refreshResult = await baseQuery("/auth/refresh", api, extraOptions)
+
+        if (refreshResult?.data) {
+            api.dispatch(setCredentials({ ...refreshResult.data }))
+
+            result = await baseQuery(args, api, extraOptions)
+        } else {
+            if (refreshResult?.error?.status === 403) {
+                refreshResult.error.data.message = "Session expired. Please log in again. "
+            }
+
+            return refreshResult
+        }
+    }
+    return result
+}
 
 export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api' }),
+    baseQuery: baseQueryWithRefreshAuth,
     tagTypes: ["User", "Item"],
     endpoints: () => ({})
 })
