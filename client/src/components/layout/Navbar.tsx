662ac13baa7868cdeaaa1925ab042dbf3880313e
Radoslav <radoslav.pavlinchov@gmail.com>
2024-07-21 19:10:24 +0300

    feat: add logout button

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -1,10 +1,49 @@
-import { NavLink } from "react-router-dom"
+// import { useEffect } from "react"
+import { NavLink, useNavigate } from "react-router-dom"
 import styles from "./Navbar.module.css"
 import navigationConfig from "../../configs/navigationConfig"
+import { useLogoutMutation } from "../../app/api/authApiSlice"
 
 // To-Do: use NavLink component to add styles dynamically due to having inside state
 
 export default function Navbar() {
+    const navigate = useNavigate()
+
+    const [logout, {
+        isLoading,
+        // isSuccess,
+        isError,
+        error
+    }] = useLogoutMutation()
+
+    // useEffect(() => {
+    //     console.log("isSuccess", isSuccess)
+
+    //     if (isSuccess) {
+    //         console.log("Logout successful")
+    //         navigate("/")
+    //     }
+    // }, [isSuccess, navigate])
+
+    if (isLoading) return <div>Logging Out...</div>
+
+    if (isError) return <div>Error: {error.data?.message}</div>
+
+    const handleLogout = async () => {
+        try {
+            await logout()
+
+            // TODO: checkout why the isSuccess is not working and better use the useEffect hook
+
+            // if (isSuccess) {
+            navigate("/")
+            // }
+
+        } catch (error) {
+            console.error("Error logging out", error)
+        }
+    }
+
     return (
         <nav className={styles.navbar}>
             <ul className={styles.navList}>
@@ -24,6 +63,14 @@ export default function Navbar() {
                 }
 
             </ul>
+
+            {/* // TODO: checkout if a button or additional NavLink is needed */}
+            <button
+                className={styles.logoutButton}
+                onClick={handleLogout}
+            >
+                Logout
+            </button>
         </nav >
     )
 }
