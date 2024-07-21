1d94c37f46c913ddb0f7469a4c413bb9b280958c
Radoslav <radoslav.pavlinchov@gmail.com>
2024-07-21 19:16:05 +0300

    feat: add login functionality, add controls to the form

--- client/src/components/pages/auth/Login.tsx
+++ client/src/components/pages/auth/Login.tsx
@@ -1,13 +1,107 @@
+import { useRef, useState, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { useDispatch } from "react-redux"
+
+import { setCredentials } from "../../../app/api/authSlice"
+import { useLoginMutation } from "../../../app/api/authApiSlice"
+
 import styles from "./Auth.module.css"
 
 export default function Login() {
+    const emailRef = useRef()
+    const errorRef = useRef()
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [errMsg, setErrMsg] = useState("")
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const [login, { isLoading }] = useLoginMutation()
+
+    useEffect(() => {
+        emailRef.current.focus()
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('')
+    }, [email, password])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const { accessToken } = await login({ email, password }).unwrap()
+
+            dispatch(setCredentials({ accessToken }))
+
+            setEmail("")
+            setPassword("")
+
+            navigate("/")
+        } catch (error) {
+            if (!error.status) {
+                return setErrMsg("Network error")
+            }
+
+            if (error.status === 400) {
+                return setErrMsg("Invalid credentials")
+            }
+
+            if (error.status === 401) {
+                return setErrMsg("Unauthorized")
+            }
+
+            setErrMsg(error.data?.message)
+
+            errorRef.current.focus()
+        }
+
+    }
+
+    const handleEmailInput = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handlePasswordInput = (e) => {
+        setPassword(e.target.value)
+    }
+
+    if (isLoading) return <div>Loading...</div>
+
     return (
         <div className={styles.auth}>
             <h2>Login</h2>
-            <form>
-                <input type="email" placeholder="Email" required />
-                <input type="password" placeholder="Password" required />
-                <button type="submit">Login</button>
+
+            <p ref={errorRef}>{errMsg}</p>
+
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        ref={emailRef}
+                        onChange={handleEmailInput}
+                        placeholder="Email"
+                        autoComplete="email" // off
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={handlePasswordInput}
+                        placeholder="Password"
+                        required
+                    />
+                </div>
+                <button>Login</button>
             </form>
         </div>
     );
