42775902f9b1953beaf37b537f48a4d13a0f8882
Radoslav <radoslav.pavlinchov@gmail.com>
2024-07-27 23:15:13 +0300

    feat: add search component to home page

--- client/src/components/pages/home/Home.tsx
+++ client/src/components/pages/home/Home.tsx
@@ -3,6 +3,16 @@ import styles from "./Home.module.css"
 export default function Home() {
     return (
         <div className={styles.home}>
+            <form action="#" className="p3 rounded-lg">
+                <input
+                    type="text"
+                    name="search"
+                    id="search"
+                    placeholder="Search..."
+                    className="p-2 rounded-lg border border-gray-400 focus:outline-none"
+                />
+            </form>
+            <button>Search</button>
             <h2>Home</h2>
             <p>Welcome to the Home page</p>
         </div>
