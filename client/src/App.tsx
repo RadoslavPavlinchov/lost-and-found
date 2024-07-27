de2256c9f5428e0d0ba7e77d0ea710217ff21f9e
Radoslav <radoslav.pavlinchov@gmail.com>
2024-07-27 23:28:09 +0300

    refactor: small css changes and add default colors to config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,64 +1,51 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom"
-// import styles from "./App.module.css"
 import routesConfig from "./configs/routesConfig"
 import Layout from "./components/layout/Layout"
 import { Provider } from "react-redux"
 import { store } from "./app/store"
 
 function createRoutes(routesConfig) {
-	return routesConfig.map(route => {
-		if (route.children) {
-			return (
-				<Route key={route.key} path={route.path} element={< route.element />}>
-					{
-						route.children.map(child => {
-							return (
-								<Route
-									key={child.key}
-									path={child.path}
-									element={
-										< child.element />
-									}
-								/>
-							)
-						})
-					}
-				</Route>
-			)
-		}
+    return routesConfig.map((route) => {
+        if (route.children) {
+            return (
+                <Route
+                    key={route.key}
+                    path={route.path}
+                    element={<route.element />}
+                >
+                    {route.children.map((child) => {
+                        return (
+                            <Route
+                                key={child.key}
+                                path={child.path}
+                                element={<child.element />}
+                            />
+                        )
+                    })}
+                </Route>
+            )
+        }
 
-		return (
-			<Route
-				key={route.key}
-				path={route.path}
-				element={
-					< route.element />
-				}
-			/>
-		)
-	})
+        return (
+            <Route
+                key={route.key}
+                path={route.path}
+                element={<route.element />}
+            />
+        )
+    })
 }
 
-function App() {
-	return (
-		<Provider store={store}>
-			<BrowserRouter>
-
-				{/* <div className={styles.app}> */}
-
-				<Routes>
-					<Route element={<Layout />}>
-						{
-							createRoutes(routesConfig)
-						}
-					</Route>
-				</Routes>
-
-				{/* </div > */}
-
-			</BrowserRouter>
-		</Provider>
-	)
+export default function App() {
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<Layout />}>
+                        {createRoutes(routesConfig)}
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </Provider>
+    )
 }
-
-export default App
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,15 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
+        colors: {
+            pink: "#EF476F",
+            yellow: "#FFD166",
+            green: "#06D6A0",
+            blue: "#118AB2",
+            darkBlue: "#073B4C",
+            white: "#ffffff",
+            transparent: "transparent",
+        },
         extend: {},
     },
     plugins: [],
