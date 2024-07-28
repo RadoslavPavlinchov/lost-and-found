ae1ad4f267694a6da3ea52144f3e3e3b95e99e08
Radoslav <radoslav.pavlinchov@gmail.com>
2024-07-28 23:46:39 +0300

    feat: add sidebar component

---
+++ client/src/components/pages/dashboard/Sidebar.tsx
@@ -0,0 +1,74 @@
+import { Link, useNavigate } from "react-router-dom"
+import { useLogoutMutation } from "../../../app/api/authApiSlice"
+
+export default function Sidebar() {
+    const navigate = useNavigate()
+    const [
+        logout,
+        {
+            isLoading,
+            // isSuccess,
+            isError,
+            error,
+        },
+    ] = useLogoutMutation()
+
+    const handleLogout = async () => {
+        try {
+            await logout()
+
+            // TODO: checkout why the isSuccess is not working and better use the useEffect hook
+
+            // if (isSuccess) {
+            navigate("/")
+            // }
+        } catch (error) {
+            console.error("Error logging out", error)
+        }
+    }
+
+    if (isLoading) return <div>Logging Out...</div>
+
+    if (isError) return <div>Error: {error.data?.message}</div>
+
+    return (
+        <div className="flex flex-col w-64 h-screen bg-blue text-white">
+            <div className="flex flex-col p-4">
+                <Link
+                    to="/dashboard/profile"
+                    className="py-2 hover:bg-gray-700"
+                >
+                    Profile
+                </Link>
+                <Link to="/dashboard/items" className="py-2 hover:bg-gray-700">
+                    Add Item
+                </Link>
+                <Link
+                    to="/dashboard/profile"
+                    className="py-2 hover:bg-gray-700"
+                >
+                    All Items
+                </Link>
+                <Link
+                    to="/dashboard/profile"
+                    className="py-2 hover:bg-gray-700"
+                >
+                    Found Items
+                </Link>
+                <Link
+                    to="/dashboard/profile"
+                    className="py-2 hover:bg-gray-700"
+                >
+                    Lost Items
+                </Link>
+                <Link
+                    to="/"
+                    className="py-2 hover:bg-gray-700"
+                    onClick={handleLogout}
+                >
+                    Logout
+                </Link>
+            </div>
+        </div>
+    )
+}
