b6b129a4de9dfbff0dc40ed5afc2b912cdea30c7
Radoslav <radoslav.pavlinchov@gmail.com>
2024-08-01 17:08:39 +0300

    chore: prettier changes

--- server/middleware/verifyJWT.js
+++ server/middleware/verifyJWT.js
@@ -10,23 +10,24 @@ const verifyJWT = (req, res, next) => {
     const token = authHeader.split(" ")[1]
 
     try {
-        jwt.verify(
-            token,
-            process.env.ACCESS_TOKEN_SECRET,
-            (err, user) => {
-                if (err) {
-                    return res.status(403).json({ msg: "Token verification failed, authorization denied." })
-                }
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+            if (err) {
+                return res.status(403).json({
+                    msg: "Token verification failed, authorization denied.",
+                })
+            }
 
-                req.id = user.userInfo.id
-                req.email = user.userInfo.email
-                req.name = user.userInfo.name
-                req.role = user.userInfo.role
+            req.id = user.userInfo.id
+            req.email = user.userInfo.email
+            req.name = user.userInfo.name
+            req.role = user.userInfo.role
 
-                next()
-            })
+            next()
+        })
     } catch (error) {
-        res.status(401).json({ msg: "Token verification failed, authorization denied." })
+        res.status(401).json({
+            msg: "Token verification failed, authorization denied.",
+        })
     }
 }
 
