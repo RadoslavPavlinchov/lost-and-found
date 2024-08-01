b51f67b0b3b076df062113c7b491f525ff743840
Radoslav <radoslav.pavlinchov@gmail.com>
2024-08-01 21:21:04 +0300

    chore: code styles from prettier

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,10 @@
 import express from "express"
-import { login, logout, register, refresh } from "../controllers/authController.js"
+import {
+    login,
+    logout,
+    register,
+    refresh,
+} from "../controllers/authController.js"
 
 const router = express.Router()
 router.post("/login", login)
