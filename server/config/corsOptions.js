d1e7b25576780fd56fc39c727db48cac74f7b61e
Radoslav <radoslav.pavlinchov@gmail.com>
2024-08-02 14:36:51 +0300

    feat: enable options to receive requests from postman

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,13 +1,20 @@
+const allowedOrigins = ["http://localhost:5173", "http://localhost:3000"]
+
 const corsOptions = {
     origin: (origin, callback) => {
-        if (origin === "http://localhost:5173" || origin === "http://localhost:3000") {
+        // const isTrustedClient = (req) => {
+        //     return req.headers["x-postman-request"] === "true"
+        // }
+        // isTrustedClient(req)
+
+        if (allowedOrigins.includes(origin) || origin === undefined) {
             callback(null, true)
         } else {
             callback(new Error("Not allowed by CORS"))
         }
     },
     credentials: true,
-    optionsSuccessStatus: 200
+    optionsSuccessStatus: 200,
 }
 
 export default corsOptions
