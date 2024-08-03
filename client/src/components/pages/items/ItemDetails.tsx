ac2c62da9611e6b2b738665008fa5e9513c81351
Radoslav <radoslav.pavlinchov@gmail.com>
2024-08-03 23:43:53 +0300

    refactor: remove unused CSS file and update ItemDetails component

--- client/src/components/pages/items/ItemDetails.tsx
+++ client/src/components/pages/items/ItemDetails.tsx
@@ -1,33 +1,117 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import styles from "./ItemDetails.module.css"
+import { useEffect, useState } from "react"
+import { Link, useNavigate, useParams } from "react-router-dom"
+import { useDeleteItemMutation } from "../../../app/api/itemsApiSlice"
 
 export default function ItemDetails() {
-    // const baseUrl = import.meta.env.BASE_URL
     const baseUrl = "http://localhost:3000/api/"
 
-    const [item, setItem] = useState({});
-    const { id } = useParams();
+    const [deleteItem, { isLoading, isSuccess, isError, error }] =
+        useDeleteItemMutation()
+
+    const navigate = useNavigate()
+
+    const [item, setItem] = useState(null)
+    const { id } = useParams()
 
     useEffect(() => {
         async function fetchData() {
             const response = await fetch(`${baseUrl}items/${id}`)
-            const itemData = await response.json();
+            const itemData = await response.json()
 
             setItem(itemData)
         }
 
-        fetchData();
-    }, [baseUrl, id]);
+        fetchData()
+    }, [baseUrl, id])
 
-    // if (!item) {
-    //     return <div className={styles.itemDetails}>No item details available</div>;
+    if (!item) {
+        return <div className="">No item details available</div>
+    }
+
+    // if (loading) {
+    //     return <div className="">Loading...</div>
     // }
 
+    // if (isError) {
+    //     return <div className="">{error.data.msg}</div>
+    // }
+
+    const handleDelete = async () => {
+        try {
+            await deleteItem(id)
+
+            if (isSuccess) {
+                navigate("/dashboard/my-items")
+            }
+
+            if (isError) {
+                console.error("Error deleting item", error)
+            }
+        } catch (error) {
+            console.error("Error logging out", error)
+        }
+    }
+
     return (
-        <div className={styles.itemDetails}>
-            <h2>{item.name}</h2>
-            <p>ID: {id}</p>
+        <div className="p-4 mx-auto mt-2">
+            <div className="bg-gray-50 rounded-lg p-4 max-w-4xl w-full">
+                <h1 className="text-2xl font-bold mb-4">{item.name}</h1>
+
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 mb-6">
+                    {item.imageUrls.map((src, index) => (
+                        <img
+                            key={index}
+                            src={src}
+                            alt={`Image ${index + 1}`}
+                            className="rounded-lg"
+                        />
+                    ))}
+                </div>
+
+                <div className="mb-6">
+                    <p className="font-bold">Description:</p>
+                    <p> {item.description}</p>
+                </div>
+
+                <div className="mb-6">
+                    <div className="mb-2">
+                        <span className="font-bold">Location:</span>{" "}
+                        {item.location}
+                    </div>
+                    <div className="mb-2">
+                        <span className="font-bold">Status:</span> {item.status}
+                    </div>
+                    <div className="mb-2">
+                        <span className="font-bold">Category:</span>{" "}
+                        {item.category}
+                    </div>
+                    <div className="mb-2">
+                        <span className="font-bold">Created:</span>{" "}
+                        {new Date(item.createdAt).toLocaleDateString()}
+                    </div>
+                    <div>
+                        <span className="font-bold">Last Updated:</span>{" "}
+                        {new Date(item.updatedAt).toLocaleDateString()}
+                    </div>
+                </div>
+
+                <div className="flex space-x-4 mb-6">
+                    <Link to="/dashboard/edit-item">
+                        <button
+                            className="bg-blue text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+                        >
+                            Edit
+                        </button>
+                    </Link>
+
+                    <button
+                        className="bg-red-700 text-white px-4 py-2 rounded-lg hover:bg-red-600"
+                        onClick={handleDelete}
+                    >
+                        Delete
+                    </button>
+                </div>
+            </div>
         </div>
-    );
-};
+    )
+}
