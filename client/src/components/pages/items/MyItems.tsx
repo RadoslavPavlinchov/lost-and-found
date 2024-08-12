d40a80eb17ff29fe896084e48a5928b71ddb469f
Radoslav <radoslav.pavlinchov@gmail.com>
2024-08-12 23:09:16 +0300

    refactor: update MyItems component to use flexbox for item layout

--- client/src/components/pages/items/MyItems.tsx
+++ client/src/components/pages/items/MyItems.tsx
@@ -23,7 +23,7 @@ export default function MyItems() {
             <div className="p-4 mx-auto mt-2">
                 <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
                     <h2>Items</h2>
-                    <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                    <div className="flex flex-wrap gap-4 p-7">
                         {ids.map((itemId) => (
                             <Item key={itemId} item={entities[itemId]} />
                         ))}
