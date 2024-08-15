16479797b3a5af3f667f1664dd6c20b4c968309d
Radoslav <radoslav.pavlinchov@gmail.com>
2024-08-15 13:48:08 +0300

    feat: update styling and structure

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -1,9 +1,27 @@
+import { Link } from "react-router-dom"
 import styles from "./Footer.module.css"
+import Logo from "./Logo"
 
 export default function Foooter() {
     return (
         <footer className={styles.footer}>
-            <p>Made with &hearts; by <a href="https://github.com/RadoslavPavlinchov">Radoslav Pavlinchov</a></p>
+            <div className={styles.column}>
+                <Logo />
+            </div>
+
+            <div className={styles.middleColumn}>
+                <p className={styles.singleLine}>
+                    Made with &hearts; by{" "}
+                    <a href="https://github.com/RadoslavPavlinchov">
+                        Radoslav Pavlinchov
+                    </a>
+                </p>
+            </div>
+
+            <div className={styles.column}>
+                <Link to="/aboutUs">About Us</Link>
+                <Link to="/contacts">Contacts</Link>
+            </div>
         </footer>
-    );
+    )
 }
